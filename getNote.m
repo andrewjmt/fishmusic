65edad0c90d21dbc3fc98ca756d8b326e88075e8
andrewjmt <ajm2209@columbia.edu>
2015-01-22 16:59:24 -0500

    Update getNote.m

--- getNote.m
+++ getNote.m
@@ -1,9 +1,8 @@
-function pitch = getNoteHorizontal(x, y, dominant)
-%GETNOTEHORIZONTAL Maps x, y position into the "horizontal" pitch structure
+function pitch = getNote(x, y, dominant)
+%GETNOTE Maps x, y position into the aligned pitch mesh structure
 %   x - floating-point x position, loosely corresponding to pitch class
 %   y - floating-point y position, loosely corresponding to register
-%   dominant - boolean indicator of whether to map x, y into the dominant
-%   structure rather than the tonic structure
+%   dominant - boolean indicator of which mesh to select
 %
 %   Pitches are represented as integers. x=y=0, dominant=1, maps to 62.
 %
@@ -11,24 +10,24 @@ function pitch = getNoteHorizontal(x, y, dominant)
 %
 %   dominant=0            x
 %                  -2 -1  0  1  2
-%                   .  .  .  .  .
+%                        ...
 %           1  ...   69 72 76 79  ...
 %        y  0  ...   57 60 64 67  ...
 %          -1  ...   45 48 52 55  ...
-%                   .  .  .  .  .
+%                        ...
 %
 %   dominant=1            x
 %                  -2 -1  0  1  2
-%                   .  .  .  .  .
+%                        ...
 %           1  ... 67 71 74 77 81 ...
 %        y  0  ... 55 59 62 65 69 ...
 %          -1  ... 43 47 50 53 57 ...
-%                   .  .  .  .  .
+%                        ...
 %
 
 if dominant
     if x < -1.5
-        pitch = getNoteHorizontal(x + 3, 0, dominant) - 10;
+        pitch = getNote(x + 3, 0, dominant) - 10;
     elseif x < -.5
         pitch = 59;
     elseif x < .5
@@ -36,11 +35,11 @@ if dominant
     elseif x < 1.5
         pitch = 65;
     else
-        pitch = getNoteHorizontal(x - 3, 0, dominant) + 10;
+        pitch = getNote(x - 3, 0, dominant) + 10;
     end
 else
     if x < -1
-        pitch = getNoteHorizontal(x + 3, 0, dominant) - 10;
+        pitch = getNote(x + 3, 0, dominant) - 10;
     elseif x < 0
         pitch = 60;
     elseif x < 1
@@ -48,7 +47,7 @@ else
     elseif x < 2
         pitch = 67;
     else
-        pitch = getNoteHorizontal(x - 3, 0, dominant) + 10;
+        pitch = getNote(x - 3, 0, dominant) + 10;
     end
 end
 
